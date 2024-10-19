1d6489e49b883cdd283644eeb45d965143501f7c
Zhuravleva0701 <shpachevamariia@gmail.com>
2024-10-20 00:08:14 +0300

    д/з 19_2

--- Django/task1/models.py
+++ Django/task1/models.py
@@ -11,7 +11,7 @@ class Buyer(models.Model):
 class Game(models.Model):
     title = models.CharField(max_length=50)
     cost = models.DecimalField(max_digits=7, decimal_places=2)
-    size = models.DecimalField(max_digits=7, decimal_places=2)
+    size = models.DecimalField(max_digits=7, decimal_places=4)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
